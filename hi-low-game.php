e68a064b40a396271905966b2b26499e631a5687
Gonzalo Rodriguez <g@stm.pm>
2017-07-19 14:05:11 -0500

    2nd version of the workign game

--- hi-low-game.php
+++ hi-low-game.php
@@ -1,1 +1,26 @@
 <?php
+do {
+  // game picks a random number between 1 and 100
+  $randomNumber =(rand(1,100));
+  // prompts user to guess the number
+  fwrite(STDOUT, 'Guess a number between 1 and 100 ');
+  // Get the input from user
+  $playerAnswer = fgets(STDIN);
+  // if user's guess is less than the number, it outputs "HIGHER"
+  // foreach(input){}
+
+    if ($playerAnswer < $randomNumber){
+    echo "HIGHER!";
+    fwrite(STDOUT, 'Guess again');
+// if user's guess is more than the number, it outputs "LOWER"
+    } else if ($playerAnswer > $randomNumber){
+    echo "LOWER!";
+    fwrite(STDOUT, 'Guess again');
+    }
+}
+while ($playerAnswer != $randomNumber);
+// if a user guesses the number, the game should declare "GOOD GUESS!"
+if ($playerAnswer == $randomNumber){
+    echo "GOOD GUESS!";
+}
+?>
